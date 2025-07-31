e7c3d6ef1989b77bcb74f7e4950bd8e5e39fde1e
Geetam09 <geetamdupare2002@outlook.com>
2025-07-31 14:11:45 +0530

    Initial commit

---
+++ Server/src/main/java/com/example/VidyaMitra/VidyaMitraApplication.java
@@ -0,0 +1,13 @@
+package com.example.VidyaMitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidyaMitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidyaMitraApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/example/VidyaMitra/VidyaMitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VidyaMitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidyaMitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
