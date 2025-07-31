53f1d14b88c43ed8ef4b423403bb0957434a4f4b
Geetam09 <geetamdupare2002@outlook.com>
2025-07-31 14:48:10 +0530

    Setup version 2

--- Server/src/main/java/com/example/VidyaMitra/Domain/Teacher/TeacherEntity.java
+++ Server/src/main/java/com/example/VidyaMitra/Domain/Teacher/TeacherEntity.java
@@ -1,4 +1,8 @@
 package com.example.VidyaMitra.Domain.Teacher;
 
+import jakarta.persistence.Entity;
+
+
+@Entity
 public class TeacherEntity {
 }
---
+++ Server/src/main/java/com/example/VidyaMitra/Domain/Teacher/TeacherServiceImp.java
@@ -0,0 +1,4 @@
+package com.example.VidyaMitra.Domain.Teacher;
+
+public class TeacherServiceImp {
+}
